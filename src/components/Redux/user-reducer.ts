6df17a3635327bc19eba499f6c6049459268d14c
IDelveis1 <ideltube@gmail.com>
2021-06-17 21:52:15 +0300

    Changed type for user-actions

--- src/components/Redux/user-reducer.ts
+++ src/components/Redux/user-reducer.ts
@@ -2,15 +2,8 @@ import { AppStateType } from './redux-store';
 import { ThunkAction } from "redux-thunk"
 import { UsersAPI } from "../../api/api"
 import { Dispatch } from 'react';
-import { PhotosType } from '../../types/types';
+import { InferActonsTypes, PhotosType } from '../../types/types';
 
-const FOLLOW = 'FOLLOW'
-const UNFOLLOW = 'UNFOLLOW'
-const SET_USERS = 'SET_USERS'
-const SET_USER_CURRENT_PAGE = 'SET_USER_CURRENT_PAGE'
-const SET_USER_TOTAL_COUNT = 'SET_USER_TOTAL_COUNT'
-const SET_PRE_LOADER = 'SET_PRE_LOADER'
-const SET_FOLLOWING_IN_PROGRESS = 'SET_FOLLOWING_IN_PROGRESS'
 
 export type UsersType = {
     id: number
@@ -31,10 +24,11 @@ let initialState = {
 
 type InitialStateType = typeof initialState
 
+type ActionsTypes = InferActonsTypes<typeof actions>
 
 const usersReducer = (state: InitialStateType = initialState, action: ActionsTypes): InitialStateType => {
     switch (action.type) {
-        case FOLLOW:
+        case 'FOLLOW':
             return (
                 {
                     ...state,
@@ -46,7 +40,7 @@ const usersReducer = (state: InitialStateType = initialState, action: ActionsTyp
                     }
                     )
                 })
-        case UNFOLLOW:
+        case 'UNFOLLOW':
             return (
                 {
                     ...state,
@@ -59,35 +53,35 @@ const usersReducer = (state: InitialStateType = initialState, action: ActionsTyp
                 }
             )
 
-        case SET_USERS:
+        case 'SET_USERS':
             return (
                 {
                     ...state,
                     users: action.users,
                 }
             )
-        case SET_USER_CURRENT_PAGE:
+        case 'SET_USER_CURRENT_PAGE':
             return (
                 {
                     ...state,
                     currentPage: action.currentPage
                 }
             )
-        case SET_USER_TOTAL_COUNT:
+        case 'SET_USER_TOTAL_COUNT':
             return (
                 {
                     ...state,
                     totalUsersCount: action.count
                 }
             )
-        case SET_PRE_LOADER:
+        case 'SET_PRE_LOADER':
             return (
                 {
                     ...state,
                     isFatching: action.isFatching
                 }
             )
-        case SET_FOLLOWING_IN_PROGRESS:
+        case 'SET_FOLLOWING_IN_PROGRESS':
             return (
                 {
                     ...state,
@@ -101,79 +95,46 @@ const usersReducer = (state: InitialStateType = initialState, action: ActionsTyp
     }
 }
 
-type FollowSuccessType = {
-    type: typeof FOLLOW
-    userId: number
+
+export const actions = {
+    followSuccess: (userId: number) => ({ type: 'FOLLOW', userId } as const),
+    unfollowSuccess: (userId: number) => ({ type: 'UNFOLLOW', userId } as const),
+    setUsers: (users: Array<UsersType>) => ({ type: 'SET_USERS', users } as const),
+    setUserCurrentPage: (currentPage: number) => ({ type: 'SET_USER_CURRENT_PAGE', currentPage } as const),
+    setUserTotalCount: (totalCount: number) => ({ type: 'SET_USER_TOTAL_COUNT', count: totalCount } as const),
+    setPreloader: (isFatching: boolean) => ({ type: 'SET_PRE_LOADER', isFatching } as const),
+    setFollowingInProgress: (isFatching: boolean, userId: number) => ({ type: 'SET_FOLLOWING_IN_PROGRESS', isFatching, userId } as const)
+
 }
 
-export const followSuccess = (userId: number): FollowSuccessType => ({ type: FOLLOW, userId });
 
-type UnfollowSuccessType = {
-    type: typeof UNFOLLOW
-    userId: number
-}
 
-export const unfollowSuccess = (userId: number): UnfollowSuccessType => ({ type: UNFOLLOW, userId });
 
-type SetUsersType = {
-    type: typeof SET_USERS
-    users: Array<UsersType>
-}
 
-export const setUsers = (users: Array<UsersType>): SetUsersType => ({ type: SET_USERS, users })
 
-type SetUserCurrentPageType = {
-    type: typeof SET_USER_CURRENT_PAGE
-    currentPage: number
-}
 
-export const setUserCurrentPage = (currentPage: number): SetUserCurrentPageType => ({ type: SET_USER_CURRENT_PAGE, currentPage })
 
-type setUserTotalCountType = {
-    type: typeof SET_USER_TOTAL_COUNT
-    count: number
-}
-
-export const setUserTotalCount = (totalCount: number): setUserTotalCountType => ({ type: SET_USER_TOTAL_COUNT, count: totalCount })
-
-type SetPreloaderType = {
-    type: typeof SET_PRE_LOADER
-    isFatching: boolean
-}
-
-export const setPreloader = (isFatching: boolean): SetPreloaderType => ({ type: SET_PRE_LOADER, isFatching })
-
-type setFollowingInProgressType = {
-    type: typeof SET_FOLLOWING_IN_PROGRESS
-    isFatching: boolean
-    userId: number
-}
-
-export const setFollowingInProgress = (isFatching: boolean, userId: number): setFollowingInProgressType => ({ type: SET_FOLLOWING_IN_PROGRESS, isFatching, userId })
-
-type ActionsTypes = FollowSuccessType | setFollowingInProgressType | SetPreloaderType | setUserTotalCountType | SetUserCurrentPageType |
-SetUsersType | UnfollowSuccessType 
 
 type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes>
 
 export const getUser = (currentPage: number, pageSize: number): ThunkType => {
     return async (dispatch: Dispatch<ActionsTypes>) => {
-        dispatch(setPreloader(true));
-        dispatch(setUserCurrentPage(currentPage))
+        dispatch(actions.setPreloader(true));
+        dispatch(actions.setUserCurrentPage(currentPage))
         let data = await UsersAPI.getUser(currentPage, pageSize);
-            dispatch(setPreloader(false));
-            dispatch(setUsers(data.items))
-            dispatch(setUserTotalCount(data.totalCount))
+            dispatch(actions.setPreloader(false));
+            dispatch(actions.setUsers(data.items))
+            dispatch(actions.setUserTotalCount(data.totalCount))
     }
 }
 
 export const follow = (userId: number): ThunkType => {
     return async (dispatch: Dispatch<ActionsTypes>) => {
-        dispatch(setFollowingInProgress(true, userId));
+        dispatch(actions.setFollowingInProgress(true, userId));
         let response = await UsersAPI.follow(userId);
             if (response.data.resultCode === 0) {
-                dispatch(setFollowingInProgress(false, userId));
-                dispatch(followSuccess(userId))
+                dispatch(actions.setFollowingInProgress(false, userId));
+                dispatch(actions.followSuccess(userId))
 
             }
 }
@@ -181,11 +142,11 @@ export const follow = (userId: number): ThunkType => {
 
 export const unfollow = (userId: number): ThunkType => {
     return async (dispatch: Dispatch<ActionsTypes>) => {
-        dispatch(setFollowingInProgress(true, userId));
+        dispatch(actions.setFollowingInProgress(true, userId));
         let response = await UsersAPI.unfollow(userId)
             if (response.data.resultCode === 0) {
-                dispatch(setFollowingInProgress(false, userId));
-                dispatch(unfollowSuccess(userId))
+                dispatch(actions.setFollowingInProgress(false, userId));
+                dispatch(actions.unfollowSuccess(userId))
             }
     }
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -23,3 +23,7 @@ export type ProfileType = {
     contacts: ContactType
     photos: PhotosType
 }
+
+type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
+
+export type InferActonsTypes<T extends {[key: string]: (...args : any[]) => any}> = ReturnType<PropertiesTypes<T>>
